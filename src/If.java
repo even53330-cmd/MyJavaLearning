f5da2c0c180b8b33a5217d0d6ed3615996df3c77
Bruker <even53330@gmail.com>
2025-12-20 16:34:59 +0100

    .

--- src/If.java
+++ src/If.java
@@ -1,5 +1,12 @@
 public class If {
     public static void main(String [] args ){
+int myInt = 5;
+if(myInt<10) {
+    System.out.println("Da,verno");
+}else if (myInt<20){
+    System.out.println("net,ne verno");
+}
+
 
 }
 }
