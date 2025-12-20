9d5fc144dcda4364d401e7b3ba924b08d2c914b6
Bruker <even53330@gmail.com>
2025-12-20 16:59:41 +0100

    .

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Vvedite chto nibut");
+        String string = s.nextLine();
+        System.out.println("Vu vveli "+string);
+    }
+}
