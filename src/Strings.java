d3f5ad852db95e2440b57d124c5c0ee39ee5a4c4
Bruker <even53330@gmail.com>
2025-12-20 15:40:57 +0100

    .

---
+++ src/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String [] args){
+        String s = "Hello";
+        String spase = " ";
+        String name = "Bob";
+        System.out.println(s+spase+name);
+    }
+}
