f8b6d231b6b555c3ac14938ab1f57d4e2bda9699
Bruker <even53330@gmail.com>
2025-12-20 15:30:34 +0100

    .

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void mein(String [] arg ){
+    public static void main(String [] args ) {
         System.out.println("HelloWorld");
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String [] args) {
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 123451234;
+
+        double myDouble = 1324.12;
+        float myFloat = 76844.1f;
+
+        char s = 'a';
+        boolean b = true;
+
+        byte myByte = 127; //-128 128
+        System.out.println(myInt);
+    }
+}
