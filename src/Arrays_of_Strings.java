c9e6aef2b867cbea196629058036803039a659db
Bruker <even53330@gmail.com>
2025-12-25 18:34:54 +0100

    .

---
+++ src/Arrays_of_Strings.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+
+public class Arrays_of_Strings {
+    public static void main(String [] args){
+        int [] numbers = new int [5];
+        numbers [0] = 10;
+        System.out.println(numbers[0]);
+            String[] strings = new String[3];
+            strings[0] = "Hello";
+            strings [1] = "bye";
+            strings[2] = "java";
+        System.out.println(strings[0]);
+        System.out.println(strings[2]);
+
+        for (int i = 0;i<strings.length;i++){
+            System.out.println(strings[i]);
+        }
+        System.out.println();
+        for(String string:strings){
+            System.out.println(string);
+        }
+        int[] numbers1 = {1,2,3};
+        int sum = 0;
+        for(int x:numbers1){
+            sum = sum+x;
+        }
+        System.out.println();
+        System.out.println(sum);
+    }
+}
